6135679eacdbe743231e4c2b76eba2b8cbcef646
Max Bilokur <bilmaxim@gmail.com>
2023-05-20 21:15:33 +0300

    added function for parsing vacancy

--- data_parser.py
+++ data_parser.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from dataclasses import dataclass, astuple, fields
@@ -15,3 +16,40 @@ class Vacancy:
     replies: int
     description: str
     link: str
+
+
+async def parse_vacancy(soup: BeautifulSoup) -> Vacancy:
+    exp = (
+        soup.select_one(".list-jobs__details__info")
+        .find(string=re.compile(r"досвіду")).split()[1]
+    )
+    if exp.isnumeric():
+        exp = int(exp)
+    else:
+        exp = 0
+
+    info = soup.select_one(".text-date").text.split()
+    replies = info.pop()
+    views = info.pop()
+    if info[0] == "сьогодні":
+        real_date = datetime.now()
+    elif info[0] == "вчора":
+        real_date = datetime.now() - timedelta(days=1)
+    else:
+        real_date = datetime.strptime(" ".join(info), "%d %B")
+
+    return Vacancy(
+        title=(
+            soup.select_one(".list-jobs__title").text
+            .strip().replace("\n", " ")
+        ),
+        posted=real_date.date(),
+        experience=exp,
+        views=int(views),
+        replies=int(replies),
+        description=soup.select_one(".text-card").text.strip(),
+        link=urljoin(
+            config.BASE_URL,
+            soup.select_one(".profile[href]")["href"]
+        )
+    )
