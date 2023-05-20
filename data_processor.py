766fb6f2dc0ca273a5b51d4fd6cef1aa4f80dd9c
Max Bilokur <bilmaxim@gmail.com>
2023-05-20 21:34:36 +0300

    separated function with text cleaning logic

--- data_processor.py
+++ data_processor.py
@@ -6,12 +6,16 @@ from nltk.probability import FreqDist
 import string
 
 
-def get_tech_count(complete_text: str, count: int) -> list[tuple]:
+def clean_text(raw_text: str) -> str:
     remove_chars = string.punctuation + "0123456789’—–"
-    complete_text = complete_text.lower().translate({ord(i): " " for i in remove_chars})
-    complete_text = re.sub(" +", " ", complete_text)
+    cleaned_text = raw_text.lower().translate({ord(i): " " for i in remove_chars})
+    cleaned_text = re.sub(" +", " ", cleaned_text)
+    return cleaned_text
 
-    words = word_tokenize(complete_text)
+
+def get_tech_count(complete_text: str, count: int) -> list[tuple]:
+    cleaned_text = clean_text(complete_text)
+    words = word_tokenize(cleaned_text)
     all_stopwords = set(stopwords.words('english'))
 
     filtered_words = [word for word in words if word.isalpha() and word not in all_stopwords]
