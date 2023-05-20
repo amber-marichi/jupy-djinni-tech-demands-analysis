b3f402e2a8c26875122824302997db84d72b23b8
Max Bilokur <bilmaxim@gmail.com>
2023-05-20 21:38:44 +0300

    function for getting stopwords logic, exteds with 2 custom files

--- data_processor.py
+++ data_processor.py
@@ -13,11 +13,20 @@ def clean_text(raw_text: str) -> str:
     return cleaned_text
 
 
+def get_stopwords() -> set:
+    all_stopwords = set(stopwords.words("english"))
+    with open("blacklist_EN.txt") as file:
+        custom_eng = [line.strip() for line in file.readlines()]
+    all_stopwords.update(custom_eng)
+    with open("blacklist_UA.txt") as file:
+        custom_ua = [line.strip() for line in file.readlines()]
+    all_stopwords.update(custom_ua)
+
+
 def get_tech_count(complete_text: str, count: int) -> list[tuple]:
     cleaned_text = clean_text(complete_text)
     words = word_tokenize(cleaned_text)
-    all_stopwords = set(stopwords.words('english'))
-
+    all_stopwords = get_stopwords()
     filtered_words = [word for word in words if word.isalpha() and word not in all_stopwords]
 
     return FreqDist(filtered_words).most_common(count)
